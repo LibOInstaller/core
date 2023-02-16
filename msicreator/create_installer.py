40feda6a4038c643c9f78f5d767f8877d0cc9939
Ximena Alcaman <flawergerl.x@gmail.com>
2023-02-16 13:24:15 -0300

    add UI graphics

--- msicreator/create_installer.py
+++ msicreator/create_installer.py
@@ -13,11 +13,17 @@ def prepare_project_dir():
     new_fonts_dir = os.path.join(creator_dir, 'libo-fonts/share/fonts')
     main_dir = os.path.join(creator_dir, 'main')
     src_uninstaller_icon = os.path.join(src_dir, 'icon-themes/colibre/res/mainapp_48_8.png')
+    src_ui_banner = os.path.join(src_dir, 'instsetoo_native/inc_common/windows/msi_templates/Binary/Banner.bmp')
+    src_ui_background = os.path.join(src_dir, 'instsetoo_native/inc_common/windows/msi_templates/Binary/Image_2.bmp')
+    graphics_dir = os.path.join(creator_dir, 'graphics')
     sdk_dir = os.path.join(creator_dir, 'main/sdk')
     try:
         move(fonts_dir, new_fonts_dir)
         copytree(instdir, main_dir, dirs_exist_ok=True)
         copy2(src_uninstaller_icon, creator_dir)
+        os.mkdir(graphics_dir)
+        copy2(src_ui_banner, graphics_dir)
+        copy2(src_ui_background, graphics_dir)
         rmtree(sdk_dir)
     except FileExistsError as err:
         print(err)
@@ -35,13 +41,16 @@ def create_creator_json():
         "name_base": lo_name,
         "comments": "Testing a libo installer",
         "installdir": "LibreOffice Test",
-        "license_file": "./License.rtf",
         "startmenu_shortcut": "program/soffice.exe",
         "desktop_shortcut": "program/soffice.exe",
         "addremove_icon": uninstaller_icon,
         "major_upgrade": {
             "AllowDowngrades": "yes",
             "IgnoreRemoveFailure": "yes"
+        },
+        "graphics": {
+            "banner": "graphics/Banner.bmp",
+            "background": "graphics/Image_2.bmp"
         },
         "parts": [
             {
@@ -64,8 +73,6 @@ def create_creator_json():
     lo_object = json.dumps(lo_dictionary, indent=4)
     with open(os.path.join(creator_dir, 'lo.json'), 'w') as lo_json:
         lo_json.write(lo_object)
-    license_file = os.path.join(src_dir, 'msicreator/License.rtf')
-    copy2(license_file, creator_dir)
 
 def generate_installer():
     os.chdir(creator_dir)
